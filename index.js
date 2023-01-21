ebfcdeaaaa8439e5fcf583e101a13bbc5bb5e2f6
Braifz <braian94lp@gmail.com>
2023-01-21 11:36:28 -0300

    query using async/await

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ const axios = require("axios");
 const ALCHEMY_URL =
   "https://eth-goerli.g.alchemy.com/v2/2H449vdDoKdwLJxsEGpmgY9f3GO-Ks6I";
 
-axios
-  .post(ALCHEMY_URL, {
-    jsonrpc: "2.0",
-    id: 1,
-    method: "eth_getBlockByNumber",
-    params: [
-      "0xb443", // block 46147
-      false, // retrieve the full transaction object in transactions array
-    ],
-  })
-  .then((response) => {
+const getBlockByNumber = async () => {
+  try {
+    const response = await axios.post(ALCHEMY_URL, {
+      jsonrpc: "2.0",
+      id: 1,
+      method: "eth_getBlockByNumber",
+      params: [
+        "pending",
+        false, // retrieve the full transaction object in transactions array
+      ],
+    });
     console.log(response.data.result);
-  });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+getBlockByNumber();
